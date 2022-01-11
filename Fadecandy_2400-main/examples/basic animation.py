3721ee287eade1bc92d41932ce04d4acbddd5a82
jx0424 <jl1280@live.mdx.ac.uk>
2022-01-11 17:02:02 +0000

    shorter code for scrolling at the same time

--- Fadecandy_2400-main/examples/basic animation.py
+++ Fadecandy_2400-main/examples/basic animation.py
@@ -16,7 +16,28 @@ for led in range(60): #pick out indeces: led= 0,1,2,3,...
 
 led = 0
 while led<60:
-    leds[led] = (0,255,0)
+    leds[59-led] = (0,255,0)
     time.sleep(.1)
     client.put_pixels(leds)
-    led = led + 1 
+    led = led + 1
+    
+#led = 59
+#while led>=0:
+ #   leds[led] = (0,255,0)
+  #  time.sleep(.1)
+   # client.put_pixels(leds)
+   # led = led - 1 
+
+led = 0
+while led<60: #scroll through all rows at the same time
+    for rows in range(6):
+        leds[led + rows*60] = (0,0,255)
+    client.put_pixels(leds)
+    time.sleep(.1)
+    led = led + 1
+
+
+# reverse the last example.
+# do a scroll from the middle to the outside - two pixels moving away from each other.
+# reverse the scroll from the middle
+# do a snake, 5 pixels long, returns to start when it hits the end
