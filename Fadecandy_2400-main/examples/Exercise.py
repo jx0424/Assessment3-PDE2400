c2b8d914e5d7d5f0a0caa791eca6a272c529d820
jx0424 <jl1280@live.mdx.ac.uk>
2022-01-11 15:10:40 +0000

    loop ofor choice

--- Fadecandy_2400-main/examples/Exercise.py
+++ Fadecandy_2400-main/examples/Exercise.py
@@ -1,9 +1,23 @@
-value = input("Welcome to the menu. Options are listed below:")
+value = input("Welcome to the menu. Options are listed below: \n\t 1. Roll \n\t 2.Sweep \n\t 3. Scroll \n Type the numbers of your choice:") #\n-new line \t-tab
 
-print("The value you input is:", value)
-print(f'it is of type{type(value)}.')
+#print("The value you input is:", value)
+#print(f'it is of type{type(value)}.')
 
-value = int(value)
+while True:
+    if value.isdigit()== True: # .isdigit() is to check if the input is a digit
+        value = int(value)
+        break #exit the loop when the correct data type is enterd.
+    else:
+        value = input("Invalid input, please provide an integer:") #ask user to reenter value
+        
+#print("The converted value is:", value)
+#print(f'it is of type{type(value)}.')
 
-print("The converted value is:", value)
-print(f'it is of type{type(value)}.')
+
+if value == 1:
+    func1(value)
+elif value ==2:
+    func2(value)
+elif value ==3:
+    func3(value)
+
