4d86f6a779d54f7d08555c19886faf81e9fc1510
jx0424 <jl1280@live.mdx.ac.uk>
2022-01-17 03:13:10 +0000

    added UI and 1 function

--- Fadecandy_2400-main/examples/Test1.py
+++ Fadecandy_2400-main/examples/Test1.py
@@ -7,3 +7,31 @@ leds=[(255,255,255)]*360
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
+
+#-----------------User input-------------------#
+user_Input = input ("Please select one of the options below: \n\t 1.Tic Tac Toe \n\t 2.RGB lights \n\t 3.Malaysia Flag \n Type the number of your choice:")
+
+while True:
+    if user_Input.isdigit() == True: #To check if the user's input is digit
+        user_Input = int (user_Input)
+        if user_Input > 6 or user_Input < 1: #To prevent user from entering value that is out of range 
+            user_Input = input ("Please enter the number between 1 and 6 only:")
+            continue #This will skip the rest of the loop and start at user_Input.isdigit to check if the input is digit.
+        else:
+            break #To exit the loop when appropriate input is entered.
+    else:
+        user_Input = input ("Please only retype a number:") #If user enter anything other than numbers, this will prompt and ask user to input number only.
+    
+#Performing the functions base on user's input.
+if user_Input == 1:
+    print (user_Input)
+elif user_Input == 2:
+    print (user_Input)
+elif user_Input == 3:
+    print (user_Input)
+elif user_Input == 4:
+    print (user_Input)
+elif user_Input == 5:
+    print (user_Input)
+elif user_Input == 6:
+    print (user_Input)
