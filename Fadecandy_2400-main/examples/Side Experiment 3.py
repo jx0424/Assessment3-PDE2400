90989f6dd41c57982947ddfc60adc74f54065a38
jx0424 <jl1280@live.mdx.ac.uk>
2022-02-08 16:34:15 +0000

    RGB vortex(endless)

--- Fadecandy_2400-main/examples/Side Experiment 3.py
+++ Fadecandy_2400-main/examples/Side Experiment 3.py
@@ -11,16 +11,17 @@ client.put_pixels(leds)
 s = 1.0
 v = 1.0
 
-for hue in range(360):
-    rgb_fractional = colorsys.hsv_to_rgb(hue/360.0, s, v)
-    r_float = rgb_fractional[0]
-    g_float = rgb_fractional[1]
-    b_float = rgb_fractional[2]
-    rgb = (r_float*255, g_float*255, b_float*255)
-    leds[hue] = rgb
-    leds[359-hue] = rgb
-    client.put_pixels(leds)
-    time.sleep(.01)
+while True:
+    for hue in range(360):
+        rgb_fractional = colorsys.hsv_to_rgb(hue/360.0, s, v)
+        r_float = rgb_fractional[0]
+        g_float = rgb_fractional[1]
+        b_float = rgb_fractional[2]
+        rgb = (r_float*255, g_float*255, b_float*255)
+        leds[hue] = rgb
+        leds[359-hue] = rgb
+        client.put_pixels(leds)
+        time.sleep(.01)
     
         
 
