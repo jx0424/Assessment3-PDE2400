f21d456ba369934353480ad565e0f362f4a679d2
jx0424 <jl1280@live.mdx.ac.uk>
2022-02-14 01:15:20 +0000

    Vortex with random colours stops when repeated 5th time

--- Fadecandy_2400-main/examples/Side Experiment 3.py
+++ Fadecandy_2400-main/examples/Side Experiment 3.py
@@ -11,17 +11,21 @@ client.put_pixels(leds)
 s = 1.0
 v = 1.0
 
+counter = 0
 while True:
     for hue in range(360):
         rgb_fractional = colorsys.hsv_to_rgb(hue/360.0, s, v)
         r_float = rgb_fractional[0]
         g_float = rgb_fractional[1]
         b_float = rgb_fractional[2]
-        rgb = (r_float*255, g_float*255, b_float*255)
+        rgb = (r_float*random.randint(1,255), g_float*random.randint(1,255), b_float*random.randint(1,255))
         leds[hue] = rgb
         leds[359-hue] = rgb
         client.put_pixels(leds)
         time.sleep(.01)
-    
-        
+    counter = counter +1
+    print ("Repeated for", counter , "time(s)")
+    if counter == 5:
+        break
 
+    
--- Fadecandy_2400-main/examples/Side experiment.py
+++ Fadecandy_2400-main/examples/Side experiment.py
@@ -7,27 +7,13 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-led = 0
 
-while led < 60:
-    for rows in range (6):
-        leds[led + rows*60] = (255,0,0)
-    time.sleep(.1)
-    client.put_pixels(leds)
-    led = led +1
-    
-led = 0
 
-while led < 60:
-    for rows in range (6):
-        leds[3 + rows*60] = (0,0,255)
-
-while led < 360:
-    led = 0
-    leds[4+led] = (0,0,255)
-    time.sleep(.1)
-    client.put_pixels(leds)
-    led = led +1
-    time.sleep(.1)
-    client.put_pixels(leds)
-    led = led +1
+#while led < 60:
+   # for rows in range (6):
+    #    leds[led + rows*60] = (255,0,0)
+    #time.sleep(.1)
+    #client.put_pixels(leds)
+   # led = led +1
+leds[10]=(0,255,0)
+client.put_pixels(leds)
