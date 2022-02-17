0fcb284722f31756bfb00dcbaa132eb592d5c016
jx0424 <jl1280@live.mdx.ac.uk>
2022-02-17 15:59:21 +0000

    added  animation 4 into the main code

--- Fadecandy_2400-main/examples/Python Assignment.py
+++ Fadecandy_2400-main/examples/Python Assignment.py
@@ -5,13 +5,13 @@ import colorsys
 from datetime import date
 from time import sleep 
 
-leds=[(255,255,255)]*360
+leds=[(0,0,0)]*360
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
 
-#---------------------Functions for Animation 1 ----------------------------
+#---------------------Functions for Animation 1(Vortex)----------------------------
 def vortex():
     #to first set the led to all white
     leds=[(255,255,255)]*360
@@ -44,7 +44,7 @@ def vortex():
         if counter == 5:
             break
 
-#-----------------------------Fucntions for Animation 2-----------------------------#
+#-----------------------------Fucntions for Animation 2 (Year 2022)-----------------------------#
 #print the letter Y
 def printY():
     for rows in range(4):
@@ -281,13 +281,8 @@ def printSad():
     client.put_pixels(leds)
     sleep(0.1)
 
-#-------------Function for Animation 3 (RainDrop)--------------------
+#-------------Function for Animation 3(RainDrop)--------------------
 def rainDrop():
-    #leds=[(255,255,255)]*360
-    #client = opc.Client('localhost:7890')
-    #client.put_pixels(leds)
-    #client.put_pixels(leds)
-
     global counter
     counter =0
     flood = 0
@@ -366,10 +361,212 @@ def accRain(counter,flood):
             sleep(0.001)
     return leds            
 
+#------------------------ Functions for Animation 4(Sun Rise and Dawn)-------------------
+def mountain():
+    while True:
+        #First mountain generation
+        for led in range(12):
+            leds[303+led]=(0,255,0)
+            leds[326-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(10):
+            leds[244+led]=(0,255,0)
+            leds[265-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(8):
+            leds[185+led]=(0,255,0)
+            leds[204-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(6):
+            leds[126+led]=(0,255,0)
+            leds[143-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(4):
+            leds[67+led]=(0,255,0)
+            leds[82-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(2):
+            leds[8+led]=(0,255,0)
+            leds[21-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        led =0
+        while led < 6:
+            leds[327+led]=(255,255,255)
+            leds[267+led]=(255,255,255)
+            leds[207+led]=(255,255,255)
+            leds[147+led]=(255,255,255)
+            leds[87+led]=(255,255,255)
+            leds[27+led]=(255,255,255)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1          
+        #2nd mountain generation
+        for led in range(12):
+            leds[333+led]=(0,255,0)
+            leds[356-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(10):
+            leds[274+led]=(0,255,0)
+            leds[295-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(8):
+            leds[215+led]=(0,255,0)
+            leds[234-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(6):
+            leds[156+led]=(0,255,0)
+            leds[173-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(4):
+            leds[97+led]=(0,255,0)
+            leds[112-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        for led in range(2):
+            leds[38+led]=(0,255,0)
+            leds[51-led]=(0,255,0)
+            client.put_pixels(leds)
+            sleep(0.01)
+            led = led +1
+        sunRise()
+        sleep(1)
+        sunDown()
+        if counter ==3:
+            break
+     
+def sunRise():
+    global counter
+    counter = 0  
+    while counter <3:
+        for led in range(2):
+            leds[254+led] = (255,0,0)
+            client.put_pixels(leds)
+            sleep(0.1)
+            led = led + 1
+        for led in range(4):
+            leds[193+led] = (255,77,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+        for led in range(6):
+            leds[132+led] = (255,103,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+        for led in range(8):
+            leds[71+led] = (255,129,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+        for led in range(10):
+            leds[10+led] = (255,167,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+#make whole board black to simulate blink            
+        for led in range(2):
+            leds[254+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(4):
+            leds[193+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(6):
+            leds[132+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(8):
+            leds[71+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(10):
+            leds[10+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        counter = counter +1
+        
+def sunDown():
+    global counter
+    counter =0
+    while counter < 3:
+        for led in range(10):
+            leds[40+led] = (255,0,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+        for led in range(8):
+            leds[101+led] = (255,77,0)
+            client.put_pixels(leds)
+            sleep(.1)            
+            led = led + 1
+        for led in range(6):
+            leds[162+led] = (255,103,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1
+        for led in range(4):
+            leds[223+led] = (255,129,0)
+            client.put_pixels(leds)
+            sleep(.1)
+            led = led + 1           
+        for led in range(2):
+            leds[284+led] = (255,167,0)
+            client.put_pixels(leds)
+            sleep(0.1)
+            led = led + 1
+#make whole board black to simulate blink            
+        for led in range(2):
+            leds[284+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(4):
+            leds[223+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(6):
+            leds[162+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(8):
+            leds[101+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        for led in range(10):
+            leds[40+led] = (0,0,0)
+            client.put_pixels(leds)
+            led = led + 1
+        print(counter)
+        counter = counter +1
+        
+
+
 
 
 #call function
 #vortex()
+mountain()
 rainDrop()
 if __name__== "__main__":
     leds=[(255,255,255)]*360
