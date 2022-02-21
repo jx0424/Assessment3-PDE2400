dc3eba95a20e6fa4fc6a896d2ba3b8c7e0538633
jx0424 <jl1280@live.mdx.ac.uk>
2022-02-21 01:52:18 +0000

    fireworks with shorten code

--- Fadecandy_2400-main/examples/Side Experiment 7.py
+++ Fadecandy_2400-main/examples/Side Experiment 7.py
@@ -14,79 +14,61 @@ while True:
         for led in range(5):
             leds[randomLoc-led*60]=(255,0,255)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             leds[randomLoc-led*60]=(0,0,0)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             led = led +1
-        leds[124]=(0,0,255)
-        leds[125]=(0,0,255)
-        leds[184]=(0,0,255)
-        leds[185]=(0,0,255)
-        leds[63]=(0,0,255)
-        leds[2]=(0,0,255)
-        leds[243]=(0,0,255)
-        leds[302]=(0,0,255)
-        leds[246]=(0,0,255)
-        leds[307]=(0,0,255)
-        leds[66]=(0,0,255)
-        leds[7]=(0,0,255)
-        client.put_pixels(leds)
-        sleep(1)
+        led=0
+        for rows in range(6):
+            leds[2+led+rows*60] = (0,255,255)
+            leds[302+led-rows*60] = (0,255,255)
+            client.put_pixels(leds)
+            led=led+1
+        sleep(0.5)
         leds= [(0,0,0)]*360
         client.put_pixels(leds)
+        sleep(0.8)
         
     if Fcounter == 2:
         for led in range(5):
             leds[randomLoc-led*60]=(255,0,255)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             leds[randomLoc-led*60]=(0,0,0)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             led = led +1
-        leds[138]=(0,0,255)
-        leds[139]=(0,0,255)
-        leds[198]=(0,0,255)
-        leds[199]=(0,0,255)
-        leds[77]=(0,0,255)
-        leds[16]=(0,0,255)
-        leds[257]=(0,0,255)
-        leds[316]=(0,0,255)
-        leds[260]=(0,0,255)
-        leds[321]=(0,0,255)
-        leds[80]=(0,0,255)
-        leds[21]=(0,0,255)
-        client.put_pixels(leds)
-        sleep(1)
+        led=0
+        for rows in range(6):
+            leds[16+led+rows*60] = (0,255,255)
+            leds[316+led-rows*60] = (0,255,255)
+            client.put_pixels(leds)
+            led=led+1
+        sleep(0.5)
         leds= [(0,0,0)]*360
         client.put_pixels(leds)
-
+        sleep(0.8)
+        
     if Fcounter == 2:
         for led in range(5):
             leds[randomLoc-led*60]=(255,0,255)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             leds[randomLoc-led*60]=(0,0,0)
             client.put_pixels(leds)
-            sleep(0.5)
+            sleep(0.3)
             led = led +1
-        leds[152]=(0,0,255)
-        leds[153]=(0,0,255)
-        leds[212]=(0,0,255)
-        leds[213]=(0,0,255)
-        leds[91]=(0,0,255)
-        leds[30]=(0,0,255)
-        leds[271]=(0,0,255)
-        leds[330]=(0,0,255)
-        leds[274]=(0,0,255)
-        leds[335]=(0,0,255)
-        leds[94]=(0,0,255)
-        leds[35]=(0,0,255)
-        client.put_pixels(leds)
-        sleep(1)
+        led=0
+        for rows in range(6):
+            leds[30+led+rows*60] = (0,255,255)
+            leds[330+led-rows*60] = (0,255,255)
+            client.put_pixels(leds)
+            led=led+1
+        sleep(0.5)
         leds= [(0,0,0)]*360
         client.put_pixels(leds)
+        sleep(0.8)
         
     if Fcounter ==3:
         for led in range(5):
@@ -97,22 +79,16 @@ while True:
             client.put_pixels(leds)
             sleep(0.5)
             led = led +1
-        leds[166]=(0,0,255)
-        leds[167]=(0,0,255)
-        leds[226]=(0,0,255)
-        leds[227]=(0,0,255)
-        leds[105]=(0,0,255)
-        leds[44]=(0,0,255)
-        leds[285]=(0,0,255)
-        leds[344]=(0,0,255)
-        leds[288]=(0,0,255)
-        leds[349]=(0,0,255)
-        leds[108]=(0,0,255)
-        leds[49]=(0,0,255)
-        client.put_pixels(leds)
-        sleep(1)
+        led=0
+        for rows in range(6):
+            leds[44+led+rows*60] = (0,255,255)
+            leds[344+led-rows*60] = (0,255,255)
+            client.put_pixels(leds)
+            led=led+1
+        sleep(0.5)
         leds= [(0,0,0)]*360
         client.put_pixels(leds)
+        sleep(0.8)
         
     Fcounter = Fcounter +1    
     if Fcounter == 4:
